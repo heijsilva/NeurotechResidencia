12021a0d7342e7fb04a46fae3237c898e51a5d0e
Ingrid Beatriz <ingridbeatriz_1@outlook.com>
2025-05-25 23:01:10 -0300

    fix: adjust function that update data from logged user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,15 +31,56 @@ const updateMe = async (req, res) => {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        const { nome, email, endereco } = req.body;
-        if (nome) user.nome = nome;
-        if (email) user.email = email;
-        
-        await user.save();
+        const {
+            nome,
+            email,
+            senha,
+            telefone,
+            telefone_contato,
+            whatsapp,
+            cidade,
+            estado,
+            cep,
+            endereco,
+            coordenadas,
+            imagem_url,
+            redes_sociais,
+            preferencias
+        } = req.body;
 
-        return res.status(200).json(user);
+        const update = {};
+
+        if (email) update.email = email;
+        if (nome) update.nome = nome;
+        if (senha) update.senha = senha;
+        if (telefone) update.telefone = telefone;
+        if (telefone_contato) update.telefone_contato = telefone_contato;
+        if (whatsapp) update.whatsapp = whatsapp;
+        if (cidade) update.cidade = cidade;
+        if (estado) update.estado = estado;
+        if (cep) update.cep = cep;
+        if (endereco) update.endereco = endereco;
+        if (coordenadas) update.coordenadas = coordenadas;
+
+        if (user.isONG()) {
+            if (imagem_url) update.imagem_url = imagem_url;
+            if (redes_sociais) update.redes_sociais = redes_sociais;
+        } else if (update.isAdotante()) {
+            if (preferencias) update.preferencias = preferencias;
+        }
+
+        const userF = await Usuario.findOneAndUpdate(
+            { _id: user.id },
+            update,
+            { new: true, runValidators: true }
+        );
+
+        return res.status(200).json(userF);
     } catch (err) {
-        return res.status(400).json(err);
+        if (err.code === 11000) {
+            return res.status(409).json({ error: err.message });
+        }
+        return res.status(500).json(err);
     }
 };
 
