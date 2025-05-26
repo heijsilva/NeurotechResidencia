1be744f9c2b0891031217773a77cb4f72325ba9a
Ingrid Beatriz <ingridbeatriz_1@outlook.com>
2025-05-25 21:20:45 -0300

    feat: create /me GET and PATCH

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,6 +9,42 @@ const getAllUsers = async (req, res) => {
     }
 };
 
+const getMe = async (req, res) => {
+    try {
+        const user = await Usuario.findOne({ _id: req.user.id });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+};
+
+const updateMe = async (req, res) => {
+    try {
+        const user = await Usuario.findOne({ _id: req.user.id });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const { nome, email, endereco } = req.body;
+        if (nome) user.nome = nome;
+        if (email) user.email = email;
+        
+        await user.save();
+
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(400).json(err);
+    }
+};
+
 export {
-    getAllUsers
+    getAllUsers,
+    getMe,
+    updateMe
 };
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import { getAllUsers } from '../../controllers/userController.js';
+import { getAllUsers, getMe, updateMe } from '../../controllers/userController.js';
 import middleware from '../../middleware/index.js';
 
 const router = Router();
 
 router.use([middleware.authenticated]);
 
-router.get('/', getAllUsers)
+router.get('/', getAllUsers);
+router.get('/me', getMe);
+router.patch('/me', updateMe);
 
 export default {
     prefix: "/users",
