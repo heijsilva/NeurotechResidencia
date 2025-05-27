819ff94bdbca00ea08659f772fd6038b58cae4a3
heijsilva <profissionaljsilva@gmail.com>
2025-05-27 14:43:48 -0300

    Teste implementacao modelo

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import bodyParser from 'body-parser';
+import routes, { authRoute } from './src/routes/index.js';
+import { global } from './src/middleware/index.js';
+import connectDB from './config/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const app = express();
+connectDB();
+
+// Static folder to serve images
+app.use('/download', express.static(path.resolve(__dirname, 'src', 'uploads')));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(global);
+
+app.use('/api' + authRoute.prefix, authRoute.router);
+
+routes.forEach((route) => {
+  app.use('/api' + route.prefix, route.router);
+});
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333');
+});
